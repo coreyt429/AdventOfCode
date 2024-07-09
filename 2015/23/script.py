77e9424006c1b5b261d319f7d93cd1e2f60d5841
Corey Thornburg <coreyt@coreyt.com>
2024-07-09 11:50:27 -0500

    2015 - 23 complete

--- 2015/23/script.py
+++ 2015/23/script.py
@@ -1,116 +1,132 @@
+"""
+Advent of code 2015 day 23
+"""
 import sys
 import re
 
 REGISTERS = {
-	'a': 0,
-	'b': 0
+    'a': 0,
+    'b': 0
 }
 
 INSTRUCTIONS = [
-	'hlf',
-	'tpl',
-	'inc',
-	'jmp',
-	'jie',
-	'jio'
+    'hlf',
+    'tpl',
+    'inc',
+    'jmp',
+    'jie',
+    'jio'
 ]
 
 PATTERN_INSTRUCTION = re.compile(r'(jmp|jio|inc|tpl|jie|hlf) *([ab])?(?:, )?([\+-]?\d+)?')
 PATTERN_JUMP_VALUE = re.compile(r'([+-])(\d+)')
 
 def read_program_from_file(file_name):
-	program = []
-	with open(file_name,'r',encoding='utf-8') as file:
-		lines = file.read().split('\n')
-	
-	for line in lines:
-		matches = PATTERN_INSTRUCTION.match(line)
-		if matches:
-			instruction = matches.group(1)
-			if instruction in ['hlf','tpl', 'inc']:
-				register = matches.group(2)
-				program.append({
-					'instruction': instruction,
-					'register': register
-				})
-			elif instruction in ['jio', 'jie']:
-				register = matches.group(2)
-				value = matches.group(3)
-				matches2 = PATTERN_JUMP_VALUE.match(value)
-				program.append({
-					'instruction': instruction,
-					'register': register,
-					'direction': matches2.group(1),
-					'value': int(matches2.group(2))
-				})
-			elif instruction in ['jmp']:
-				print(line)
-				print(matches)
-				print(matches.groups())
-				value = matches.group(3)
-				matches2 = PATTERN_JUMP_VALUE.match(value)
-				program.append({
-					'instruction': instruction,
-					'direction': matches2.group(1),
-					'value': int(matches2.group(2))
-				})
-			else:
-				print(f"unkown instruction in: {line}")
-		else:
-			print(f"unkown instruction in: {line}")
-			
-	print(program)
-	return program
+    """
+    Function to read filename and parse the program instructions
+
+    parameters:
+        file_name: string name of file to load
+
+    returns:
+        program: list of dict, program instructions
+    """
+    program = []
+    with open(file_name,'r',encoding='utf-8') as file:
+        lines = file.read().split('\n')
+
+    for line in lines:
+        matches = PATTERN_INSTRUCTION.match(line)
+        if matches:
+            instruction = matches.group(1)
+            if instruction in ['hlf','tpl', 'inc']:
+                register = matches.group(2)
+                program.append({
+                    'instruction': instruction,
+                    'register': register
+                })
+            elif instruction in ['jio', 'jie']:
+                register = matches.group(2)
+                value = matches.group(3)
+                matches2 = PATTERN_JUMP_VALUE.match(value)
+                program.append({
+                    'instruction': instruction,
+                    'register': register,
+                    'direction': matches2.group(1),
+                    'value': int(matches2.group(2))
+                })
+            elif instruction in ['jmp']:
+                print(line)
+                print(matches)
+                print(matches.groups())
+                value = matches.group(3)
+                matches2 = PATTERN_JUMP_VALUE.match(value)
+                program.append({
+                    'instruction': instruction,
+                    'direction': matches2.group(1),
+                    'value': int(matches2.group(2))
+                })
+            else:
+                print(f"unkown instruction in: {line}")
+        else:
+            print(f"unkown instruction in: {line}")
+
+    print(program)
+    return program
 
 def run_progam():
-	pointer = 0
-	sentinel = 0
-	threshold = 100000
-	while 0 <= pointer < len(PROGRAM):
-		sentinel+=1
-		if sentinel > threshold:
-			print("runaway?")
-			break
-		current_line = PROGRAM[pointer]
-		if current_line['instruction'] == 'inc':
-			REGISTERS[current_line['register']]+=1
-			pointer+=1
-		elif current_line['instruction'] == 'tpl':
-			REGISTERS[current_line['register']]*=3
-			pointer+=1
-		elif current_line['instruction'] == 'hlf':
-			REGISTERS[current_line['register']] =int(REGISTERS[current_line['register']]/2)
-			pointer+=1
-		elif current_line['instruction'] == 'jmp':
-			if current_line['direction'] == '+':
-				pointer+=current_line['value']
-			else:
-				pointer-=current_line['value']
-		elif current_line['instruction'] == 'jie':
-			if REGISTERS[current_line['register']] % 2 == 0:
-				if current_line['direction'] == '+':
-					pointer+=current_line['value']
-				else:
-					pointer-=current_line['value']
-			else:
-				pointer += 1
-		elif current_line['instruction'] == 'jio':
-			if REGISTERS[current_line['register']] % 2 != 0:
-				if current_line['direction'] == '+':
-					pointer+=current_line['value']
-				else:
-					pointer-=current_line['value']
-			else:
-				pointer += 1
-		print(f"After: ({current_line}): pointer: {pointer}, REGISTERS: {REGISTERS}")
-
-
-
-		
+    """
+    Executes the program instructions in PROGRAM
+    """
+    pointer = 0
+    sentinel = 0
+    threshold = 100000
+    while 0 <= pointer < len(PROGRAM):
+        sentinel+=1
+        if sentinel > threshold:
+            print("runaway?")
+            break
+        current_line = PROGRAM[pointer]
+        if current_line['instruction'] == 'inc':
+            REGISTERS[current_line['register']]+=1
+            pointer+=1
+        elif current_line['instruction'] == 'tpl':
+            REGISTERS[current_line['register']]*=3
+            pointer+=1
+        elif current_line['instruction'] == 'hlf':
+            REGISTERS[current_line['register']] =int(REGISTERS[current_line['register']]/2)
+            pointer+=1
+        elif current_line['instruction'] == 'jmp':
+            if current_line['direction'] == '+':
+                pointer+=current_line['value']
+            else:
+                pointer-=current_line['value']
+        elif current_line['instruction'] == 'jie':
+            if REGISTERS[current_line['register']] % 2 == 0:
+                if current_line['direction'] == '+':
+                    pointer+=current_line['value']
+                else:
+                    pointer-=current_line['value']
+            else:
+                pointer += 1
+        elif current_line['instruction'] == 'jio':
+            if REGISTERS[current_line['register']] == 1:
+                if current_line['direction'] == '+':
+                    pointer+=current_line['value']
+                else:
+                    pointer-=current_line['value']
+            else:
+                pointer += 1
+        print(f"After: ({current_line}): pointer: {pointer}, REGISTERS: {REGISTERS}")
 
 if __name__ == '__main__':
-	print(REGISTERS)
-	print(INSTRUCTIONS)
-	PROGRAM = read_program_from_file(sys.argv[1])
-	run_progam()
-	print(REGISTERS)
+    print(REGISTERS)
+    print(INSTRUCTIONS)
+    PROGRAM = read_program_from_file(sys.argv[1])
+    run_progam()
+    PART1_RESULT = REGISTERS['b']
+    REGISTERS['a'] = 1
+    REGISTERS['b'] = 0
+    run_progam()
+    PART2_RESULT = REGISTERS['b']
+    print(f"Part 1: {PART1_RESULT}\nPart 2: {PART2_RESULT}")
