21ef79d629a8d0e2ee54c17208cb5772ffc4381f
Corey Thornburg <coreyt@coreyt.com>
2024-08-02 16:44:26 -0500

    2016 1

--- 2015/25/solution.py
+++ 2015/25/solution.py
@@ -1,25 +1,49 @@
 """
-Advent Of Code 2015 day 25
+Advent Of Code 2015 25 25
+
+This was another one that was already fast, and clean. Just changed
+it to read the input file and use current structure.
 
 """
 # import system modules
 import time
+import re
+
 
 # import my modules
 import aoc # pylint: disable=import-error
 
-def solve(input_value, part):
+def solve(part):
     """
     Function to solve puzzle
     """
-    return part
+    if part == 2:
+        return "Press the button"
+    current_code = START_CODE
+    row = 1
+    col = 1
+    next_row = 2
+    #print(current_code,row,col)
+    previous_code = current_code
+    while True:
+        row -= 1
+        col += 1
+        if row < 1:
+            row = next_row
+            next_row += 1
+            col = 1
+        current_code = (previous_code * MULTIPLIER) % DIVISOR
+        if row == TARGET_ROW and col == TARGET_COL:
+            return current_code
+        previous_code = current_code
 
 if __name__ == "__main__":
     my_aoc = aoc.AdventOfCode(2015,25)
-    #input_text = my_aoc.load_text()
-    #print(input_text)
-    input_lines = my_aoc.load_lines()
-    print(input_lines)
+    input_text = my_aoc.load_text()
+    TARGET_ROW, TARGET_COL = (int(input) for input in re.findall(r'(\d+)', input_text))
+    START_CODE=20151125
+    MULTIPLIER=252533
+    DIVISOR=33554393
     # parts dict to loop
     parts = {
         1: 1,
@@ -40,7 +64,7 @@ if __name__ == "__main__":
         # log start time
         start_time = time.time()
         # get answer
-        answer[my_part] = funcs[part](input_lines, my_part)
+        answer[my_part] = funcs[my_part](my_part)
         # log end time
         end_time = time.time()
         # print results
