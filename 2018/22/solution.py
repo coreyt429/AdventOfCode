b5d1996d59f62a00debc7c89a5390fca76cabe62
Corey Thornburg <coreyt@coreyt.com>
2024-09-09 09:59:58 -0500

    2018 22

---
+++ 2018/22/solution.py
@@ -0,0 +1,194 @@
+"""
+Advent Of Code 2018 day 22
+
+
+Part 1 is complete.   
+
+The rules on this one made it fairly clear that recursion was going to be
+necessary, and also repetitive.  So I used functools.lru_cache to cache the
+repetitive values.
+
+This assumption paid off, when I ran without it even with the test data, it
+was significantly slower.
+
+Time to get to work, so just making some notes for part 2.
+
+Part 2 is a shortest path problem.  However, it is conditional, so the Grid()
+built in shortest_path is likely not going to work, unless we implement a callback
+to get the conditionals and h_score calculations.  This may pay dividens in the future,
+but needs to be done in a manner that doesn't break existing code.
+
+If you go this route, I'm thinking kwargs for:
+    h_score_callback - would need to pass grid, pos, and target at minimum
+    neighbor_selection_callback - would need to pass grid, pos, target, and possible neighbors
+
+"""
+# import system modules
+import time
+import functools
+
+# import my modules
+import aoc # pylint: disable=import-error
+from grid import Grid # pylint: disable=import-error
+
+# map area_types
+area_type = {
+    0: '.',
+    1: '=',
+    2: '|'
+}
+
+# reverse map to risk_level
+risk_level =  {v: k for k, v in area_type.items()}
+
+def parse_input(lines):
+    """
+    Parse input date
+    Args:
+        lines: list()
+    Returns:
+        depth: int()
+        target: tuple(x/y)
+    """
+    depth = int(lines[0].split(' ')[1])
+    target = lines[1].split(' ')[1]
+    x_pos, y_pos = [int(pos) for pos in target.split(',')]
+    return depth, (x_pos, y_pos)
+
+@functools.lru_cache(maxsize=None)
+def get_geologic_index(pos, depth, target):
+    """
+    Funciton to calculate geologic index
+    Args:
+        pos: tuple() -  int(x), int(y)
+        depth: int()
+        target: tuple() -  int(x), int(y)
+    Returns:
+        geologic_index: int()
+    """
+    # The region at 0,0 (the mouth of the cave) has a geologic index of 0.
+    # The region at the coordinates of the target has a geologic index of 0.
+    if pos in [(0,0), target]:
+        return 0
+    if pos[1] == 0:
+        # If the region's Y coordinate is 0, the geologic index is its X coordinate times 16807.
+        return pos[0] * 16807
+    # If the region's X coordinate is 0, the geologic index is its Y coordinate times 48271.
+    if pos[0] == 0:
+        return pos[1] * 48271
+    # Otherwise, the region's geologic index is the result of multiplying the erosion levels of
+    # the regions at X-1,Y and X,Y-1.
+    erosion_level_1 = get_erosion_level((pos[0] - 1, pos[1]), depth, target)
+    erosion_level_2 = get_erosion_level((pos[0], pos[1] - 1), depth, target)
+    return erosion_level_1 * erosion_level_2
+
+@functools.lru_cache(maxsize=None)
+def get_erosion_level(pos, depth, target):
+    """
+    Function to calculate erosion level for an area
+    Args:
+        pos: tuple() -  int(x), int(y)
+        depth: int()
+        target: tuple() -  int(x), int(y)
+    
+    Returns:
+        erosion_level: int()
+    """
+    # A region's erosion level is its geologic index plus the cave system's depth, all modulo 20183.
+    geologic_index = get_geologic_index(pos, depth, target)
+    return (geologic_index + depth) % 20183
+
+@functools.lru_cache(maxsize=None)
+def get_area_type(pos, depth, target):
+    """
+    function to get area type
+    Args:
+        pos: tuple() -  int(x), int(y)
+        depth: int()
+        target: tuple() -  int(x), int(y)
+    Returns:
+        area_type: char() '.', '=', or '|'
+    """
+    # If the erosion level modulo 3 is 0, the region's type is rocky.
+    # If the erosion level modulo 3 is 1, the region's type is wet.
+    # If the erosion level modulo 3 is 2, the region's type is narrow.
+    erosion_level = get_erosion_level(pos, depth, target)
+    return area_type[erosion_level % 3]
+
+def get_risk_level(grid, start, goal):
+    """
+    Function to calculate risk level
+    Args:
+        grid: Grid() current map
+        start: tuple() start position x/y coordinates
+        goal: tuple() end position x/y coordinates
+    Returns:
+        total: int() risk_level total
+    """
+    total = 0
+    min_x = min(start[0], goal[0])
+    min_y = min(start[1], goal[1])
+    max_x = max(start[0], goal[0])
+    max_y = max(start[1], goal[1])
+    for point in grid:
+        if min_x <= point[0] <= max_x and min_y <= point[1] <= max_y:
+            total += risk_level[grid.get_point(point)]
+    return total
+
+def init_grid(depth, target):
+    """
+    Function to initialize grid and populate data:
+    Args:
+        depth: int()
+        target: tuple(x, y)
+    Returns:
+        grid: Grid()
+    """
+    grid = Grid('M', type="infinite", coordinate_system='screen', use_overrides=False)
+    # populate grid
+    for x_pos in range(target[0] + 6):
+        for y_pos in range(target[1] + 6):
+            grid.set_point((x_pos, y_pos), get_area_type((x_pos, y_pos), depth, target))
+    grid.update()
+    return grid
+
+def solve(input_value, part):
+    """
+    Function to solve puzzle
+    """
+    # load input data
+    depth, target = parse_input(input_value)
+    # init grid
+    grid = init_grid(depth, target)
+    if part == 1:
+        return get_risk_level(grid, (0,0), target)
+    return part
+
+if __name__ == "__main__":
+    my_aoc = aoc.AdventOfCode(2018,22)
+    input_lines = my_aoc.load_lines()
+    # parts dict to loop
+    parts = {
+        1: 1,
+        2: 2
+    }
+    # dict to store answers
+    answer = {
+        1: None,
+        2: None
+    }
+    # dict to map functions
+    funcs = {
+        1: solve,
+        2: solve
+    }
+    # loop parts
+    for my_part in parts:
+        # log start time
+        start_time = time.time()
+        # get answer
+        answer[my_part] = funcs[my_part](input_lines, my_part)
+        # log end time
+        end_time = time.time()
+        # print results
+        print(f"Part {my_part}: {answer[my_part]}, took {end_time-start_time} seconds")
