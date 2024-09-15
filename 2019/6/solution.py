b07f9f8eb7d8e7c58f5005a2ba6f792ceb97ca15
Corey Thornburg <coreyt@coreyt.com>
2024-09-15 15:52:44 -0500

    2019 6

--- 2019/6/solution.py
+++ 2019/6/solution.py
@@ -1,9 +1,20 @@
 """
 Advent Of Code 2019 day 6
 
+This one was pretty easy.  Part 1, used a simple class structure
+to establish parent/child relationships, then just had calc_orbits
+recurse the tree to update values.  There are some unnecessary redundant
+calculations, and it runs in 0.04 seconds, so not worth the effort
+to reduce these.
+
+Part 2, had to add __lt__ to the class to use heapq for Dijkstra
+algorithm.  I probably should have used networkx here, and again
+it runs in 0.05 seconds with a manual Dijkstra solution so I'll take
+it as good that I remember teh algorithm.
 """
 # import system modules
 import time
+from heapq import heappop, heappush
 
 # import my modules
 import aoc # pylint: disable=import-error
@@ -37,6 +48,10 @@ class CelestialObject():
             self.indirect_orbits += child.calc_orbits()
         return self.direct_orbits + self.indirect_orbits
 
+    def __lt__(self, other):
+        """less than, needed for heapq"""
+        return self.oid < other.oid
+
     def __str__(self):
         """string"""
         return f"{self.oid}: direct {self.direct_orbits}, indirect {self.indirect_orbits}"
@@ -60,19 +75,68 @@ def load_objects(lines):
     # return set
     return objects
 
+
+def shortest_path(start, goal):
+    """
+    Function to find shortest path
+    """
+    # init heap
+    heap = []
+    # init visited
+    visited = set()
+    # add start point
+    heappush(heap, (0, start))
+    # init min_steps
+    min_steps = float('infinity')
+    # process heap
+    while heap:
+        # get next item
+        steps, current = heappop(heap)
+        # did we find the end?
+        if current == goal:
+            # update min_steps
+            min_steps = min(min_steps, steps)
+            # next item
+            continue
+
+        # have we already processed this node?
+        if current in visited:
+            # next item
+            continue
+
+        # have we gone too far?
+        if steps > min_steps:
+            # next item
+            continue
+
+        # add to visited
+        visited.add(current)
+        # add parent
+        if current.parent:
+            heappush(heap, (steps + 1, current.parent))
+        # add children
+        for child in current.children:
+            heappush(heap, (steps + 1, child))
+    return min_steps
+
 def solve(input_value, part):
     """
     Function to solve puzzle
     """
-    if part == 2:
-        # some madness
-        return part
+    # parse input
     objects = load_objects(input_value)
     # orbit_count = objects['COM'].calc_orbits()
     orbit_count = 0
     for obj in objects.values():
         # orbit_count += obj.direct_orbits + obj.indirect_orbits
         orbit_count += obj.calc_orbits()
+    # part 2
+    if part == 2:
+        # return shortest_path
+        return shortest_path(
+            objects['YOU'].parent,
+            objects['SAN'].parent
+        )
     return orbit_count
 
 if __name__ == "__main__":
