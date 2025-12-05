dfddd55a165f5924c5f941fd61501f3d403c1cbd
Corey Thornburg <coreyt@coreyt.com>
2025-12-05 09:53:13 -0600

    2025.5

--- 2025/5/solution.py
+++ 2025/5/solution.py
@@ -19,18 +19,67 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def parse_input(input_value):
+    """
+    Function to parse input
+    """
+    fresh_txt, ingredients_text = input_value.split("\n\n")
+    fresh = set()
+    ingredients = []
+    for fresh_range in fresh_txt.splitlines():
+        start, end = fresh_range.split("-")
+        fresh.add(tuple([int(start), int(end)]))
+    for ingredient in ingredients_text.splitlines():
+        ingredients.append(int(ingredient))
+    return fresh, ingredients
+
+
+def consolidate_ranges(ranges):
+    """
+    Function to consolidate overlapping ranges
+    """
+    sorted_ranges = sorted(ranges, key=lambda x: x[0])
+    consolidated = []
+    current_start, current_end = sorted_ranges[0]
+    for start, end in sorted_ranges[1:]:
+        if start <= current_end:
+            current_end = max(current_end, end)
+        else:
+            consolidated.append((current_start, current_end))
+            current_start, current_end = start, end
+    consolidated.append((current_start, current_end))
+    return tuple(consolidated)
+
+
 def solve(input_value, part):
     """
     Function to solve puzzle
     """
-    return part
+    fresh, ingredients = parse_input(input_value)
+    counter = 0
+    if part == 2:
+        consolidated = consolidate_ranges(fresh)
+        for start, end in consolidated:
+            logger.debug("Consolidated range: %d-%d", start, end)
+            counter += end - start + 1
+        logger.debug("Total fresh ingredients: %d", counter)
+        return counter
+    logger.debug("Fresh: %s", fresh)
+    logger.debug("Ingredients: %s", ingredients)
+    counter = 0
+    for ingredient in ingredients:
+        for fresh_range in fresh:
+            if fresh_range[0] <= ingredient <= fresh_range[1]:
+                counter += 1
+                break
+    return counter
 
 
 YEAR = 2025
 DAY = 5
 input_format = {
-    1: "lines",
-    2: "lines",
+    1: "text",
+    2: "text",
 }
 
 funcs = {
