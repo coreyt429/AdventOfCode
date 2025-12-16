ecb9059f8eef52c6ab4c6b876bb5f678b88d43c2
Corey Thornburg <coreyt@coreyt.com>
2025-12-16 12:39:43 -0600

    2023 refactor

--- 2023/19/solution.py
+++ 2023/19/solution.py
@@ -6,6 +6,7 @@ Advent Of Code 2023 day 19
 # import system modules
 import logging
 import argparse
+import re
 
 # import my modules
 from aoc import AdventOfCode  # pylint: disable=import-error
@@ -18,18 +19,186 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def parse_input(
+    data: str,
+) -> tuple[dict[str, tuple[str, ...]], list[tuple[str, str, str, str]]]:
+    """parse input data into rules and parts"""
+    # Split the data into lines
+    rule_block, part_block = data.strip().split("\n\n")
+
+    rules = {}
+    parts = []
+    # 'px{a<2006:qkq,m>2090:A,rfg}'split('{|}')
+    # {x=2127,m=1623,a=2188,s=1013}
+    for rule in rule_block.split("\n"):
+        split_characters = r"\{|\}"  # Escaping '{' and '}' and using '|' for 'or'
+        # Splitting the string
+        rule_name, rule_tests, dummy = re.split(split_characters, rule)
+        rule_list = rule_tests.split(",")
+        rules[rule_name] = tuple(rule_list)
+
+    pattern = r"{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}"
+    parts = [match.groups() for match in re.finditer(pattern, part_block) if match]
+    return rules, parts
+
+
+def accepted_part(
+    part: tuple[str, str, str, str], rules: dict[str, tuple[str, ...]], current="in"
+):
+    """determine if part is accepted according to rules"""
+    if current == "A":
+        return True
+    if current == "R":
+        return False
+    logger.debug("Evaluating part %s with rule %s", part, current)
+    logger.debug("Current rules: %s", rules)
+    rule = rules[current]
+    for test in rule:
+        matches = re.match(r"^([a-zA-Z]+)$", test)
+        if matches:
+            label = matches.group(1)
+            return accepted_part(part, rules, label)
+        comparison, label = test.split(":")
+        matches = re.match(r"([xmas])([<>])(\d+)", comparison)
+        mykey = {"x": 0, "m": 1, "a": 2, "s": 3}
+        if matches:
+            prop = matches.group(1)
+            oper = matches.group(2)
+            value = int(matches.group(3))
+            if oper == "<":
+                if int(part[mykey[prop]]) < value:
+                    return accepted_part(part, rules, label)
+            elif oper == ">":
+                if int(part[mykey[prop]]) > value:
+                    return accepted_part(part, rules, label)
+            else:
+                logger.warning(
+                    "unkown operator in comparison: %s, label: %s",
+                    comparison,
+                    label,
+                )
+    return None
+
+
+def part_value(parts: tuple[str, str, str, str]):
+    """calculate part value"""
+    pattern = r"(\d+)"
+    total_sum = 0
+    for part in parts:
+        total_sum += sum(int(match.group()) for match in re.finditer(pattern, part))
+    return total_sum
+
+
+def both(
+    ch: str,
+    gt: bool,
+    val: int,
+    ranges: list[
+        tuple[tuple[int, int], tuple[int, int], tuple[int, int], tuple[int, int]]
+    ],
+) -> list[tuple[tuple[int, int], tuple[int, int], tuple[int, int], tuple[int, int]]]:
+    """Apply both conditions to ranges"""
+    ch = "xmas".index(ch)
+    ranges2 = []
+    for rng in ranges:
+        rng = list(rng)
+        lo, hi = rng[ch]
+        if gt:
+            lo = max(lo, val + 1)
+        else:
+            hi = min(hi, val - 1)
+        if lo > hi:
+            continue
+        rng[ch] = (lo, hi)
+        ranges2.append(tuple(rng))
+    return ranges2
+
+
+def acceptance_ranges_outer(
+    work: str, rules: dict[str, tuple[str, ...]]
+) -> list[tuple[tuple[int, int], tuple[int, int], tuple[int, int], tuple[int, int]]]:
+    """Get acceptance ranges for a workflow"""
+    logger.debug("acceptance_ranges_outer: %s %s", work, rules[work])
+    return acceptance_ranges_inner(rules[work], rules)
+
+
+def acceptance_ranges_inner(
+    w: tuple[str, ...], rules: dict[str, tuple[str, ...]]
+) -> list[tuple[tuple[int, int], tuple[int, int], tuple[int, int], tuple[int, int]]]:
+    """Get acceptance ranges for a workflow inner"""
+    logger.debug("acceptance_ranges_outer(%s)", w)
+    # acceptance_ranges_outer(('s<1351:px', 'qqz'))
+    it = w[0]
+    logger.debug("it: %s", it)
+    if it == "R":
+        return []
+    if it == "A":
+        return [((1, 4000), (1, 4000), (1, 4000), (1, 4000))]
+    if ":" not in it:
+        return acceptance_ranges_outer(it, rules)
+    cond = it.split(":")[0]
+    logger.debug("cond: %s", cond)
+    gt = ">" in cond
+    logger.debug("gt: %s", gt)
+    ch = cond[0]
+    logger.debug("ch: %s", ch)
+    val = int(cond[2:])
+    logger.debug("val: %d", val)
+    val_inverted = val + 1 if gt else val - 1
+    logger.debug("val_inverted: %d", val_inverted)
+    if_cond_is_true = both(
+        ch, gt, val, acceptance_ranges_inner([it.split(":")[1]], rules)
+    )
+    if_cond_is_false = both(
+        ch, not gt, val_inverted, acceptance_ranges_inner(w[1:], rules)
+    )
+    return if_cond_is_true + if_cond_is_false
+
+
+def part1(parts: list[tuple[str, str, str, str]], rules: dict[str, tuple[str, ...]]):
+    """solve part 1 of puzzle"""
+    retval = 0
+    for part in parts:
+        if accepted_part(part, rules):
+            value = part_value(part)
+            retval += value
+    return retval
+
+
+def part2(rules: dict[str, tuple[str, ...]]):
+    """solve part 2 of puzzle"""
+    retval = 0
+    for rng in acceptance_ranges_outer("in", rules):
+        logger.debug("Range: %s", rng)
+        v = 1
+        for lo, hi in rng:
+            logger.debug("%d - %d", hi, lo)
+            v *= hi - lo + 1
+            logger.debug("%d *= %d - %d + 1", v, hi, lo)
+        retval += v
+    return retval
+
+
 def solve(input_value, part):
     """
     Function to solve puzzle
     """
-    return part
+    rules, parts = parse_input(input_value)
+    logger.debug("Parsed rules: %s", rules)
+    logger.debug("Parsed parts: %s", parts)
+    retval = 0
+    if part == 1:
+        retval = part1(parts, rules)
+    else:
+        retval = part2(rules)
+    return retval
 
 
 YEAR = 2023
 DAY = 19
 input_format = {
-    1: "lines",
-    2: "lines",
+    1: "text",
+    2: "text",
 }
 
 funcs = {
--- 2023/20/solution.py
+++ 2023/20/solution.py
@@ -6,6 +6,8 @@ Advent Of Code 2023 day 20
 # import system modules
 import logging
 import argparse
+from collections import defaultdict
+import math
 
 # import my modules
 from aoc import AdventOfCode  # pylint: disable=import-error
@@ -18,11 +20,131 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def parse_input(lines: list[str]) -> dict:
+    """parse input into data structure"""
+    machine = {}
+    for line in lines:
+        src, dst = line.split(" -> ")
+        dst = dst.split(", ")
+
+        if src == "broadcaster":
+            node_type = None
+        else:
+            node_type = src[0]
+            src = src[1:]
+
+        assert src not in machine
+        machine[src] = (node_type, dst)
+    return machine
+
+
+def set_input_map(data: dict) -> dict:
+    """Set input map"""
+    input_map = defaultdict(list)
+
+    for node, (_, dests) in data.items():
+        for d in dests:
+            input_map[d].append(node)
+    return input_map
+
+
+def set_memory(data: dict, input_map: dict) -> dict:
+    """Set initial memory"""
+    memory = {}
+
+    for node, (t, _) in data.items():
+        if t is None:
+            continue
+        if t == "%":
+            memory[node] = False
+        if t == "&":
+            memory[node] = {d: False for d in input_map[node]}
+    return memory
+
+
+def process_node(src, node, is_high_pulse, data, memory):
+    """Process a single node"""
+    info = data.get(node)
+    if info is None:
+        return []
+    t, dests = info
+    if t == "%":
+        if is_high_pulse:
+            return []
+        state = memory[node]
+        memory[node] = not state
+        return [(node, d, not state) for d in dests]
+    if t == "&":
+        state = memory[node]
+        state[src] = is_high_pulse
+        # All are high, send a low pulse
+        to_send = not sum(state.values()) == len(state)
+        return [(node, d, to_send) for d in dests]
+
+    if t is None:
+        return [(node, d, is_high_pulse) for d in dests]
+    assert False
+
+
+def part1(data: dict) -> int:
+    """solve part 1"""
+    counters = defaultdict(int)
+    input_map = set_input_map(data)
+    memory = set_memory(data, input_map)
+    for _ in range(1000):
+        todo = [(None, "broadcaster", False)]
+        while todo:
+            new_todo = []
+            for src, node, is_high_pulse in todo:
+                counters[is_high_pulse] += 1
+                new_todo.extend(process_node(src, node, is_high_pulse, data, memory))
+            todo = new_todo
+    return math.prod(counters.values())
+
+
+def part2(data: dict) -> int:
+    """solve part 2"""
+    counters = defaultdict(int)
+    input_map = set_input_map(data)
+    memory = set_memory(data, input_map)
+    assert len(input_map["rx"]) == 1
+    single = input_map["rx"][0]
+    assert data[single][0] == "&"
+    sources = input_map[single]
+    assert all(data[s][0] == "&" for s in sources)
+    # Assume that there are n sub-trees and they each have their own cycle
+    low_counts = {}
+    cycle = 0
+    while len(low_counts) < len(sources):
+        cycle += 1
+        todo = [(None, "broadcaster", False)]
+
+        while todo:
+            new_todo = []
+
+            for src, node, is_high_pulse in todo:
+                if all([node in sources, not is_high_pulse, node not in low_counts]):
+                    logger.debug("Node %s low at cycle %d", node, cycle)
+                    low_counts[node] = cycle
+
+                if node == "rx" and not is_high_pulse:
+                    rx_count += 1
+                counters[is_high_pulse] += 1
+                new_todo.extend(process_node(src, node, is_high_pulse, data, memory))
+            todo = new_todo
+
+    return math.lcm(*low_counts.values())
+
+
 def solve(input_value, part):
     """
     Function to solve puzzle
     """
-    return part
+    machine = parse_input(input_value)
+    logging.debug("Parsed machine with %d nodes", len(machine))
+    if part == 1:
+        return part1(machine)
+    return part2(machine)
 
 
 YEAR = 2023
