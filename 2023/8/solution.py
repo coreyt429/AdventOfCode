e90e609cfe1724b9563ede8bcb8e7a232a01a583
Corey Thornburg <coreyt@coreyt.com>
2025-12-15 19:53:15 -0600

    2023 refactor

--- 2023/8/solution.py
+++ 2023/8/solution.py
@@ -6,6 +6,9 @@ Advent Of Code 2023 day 8
 # import system modules
 import logging
 import argparse
+import re
+import math
+from functools import reduce
 
 # import my modules
 from aoc import AdventOfCode  # pylint: disable=import-error
@@ -18,18 +21,103 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def parse_input(data: str) -> dict:
+    """parse input data into structured format"""
+    retval = {}
+    # Split the data into lines
+    parts = data.strip().split("\n\n")
+
+    retval["instructions"] = parts[0]
+
+    map_data = {}
+    map_regex = r"(...) = \((...), (...)\)"
+    for line in parts[1].split("\n"):
+        # print(line)
+        coordinates = re.findall(map_regex, line)
+        # print(coordinates)
+        map_data[coordinates[0][0]] = {"L": coordinates[0][1], "R": coordinates[0][2]}
+    retval["map"] = map_data
+    return retval
+
+
+def part1(parsed_data: dict) -> int:
+    """Solve part 1 of the puzzle"""
+    steps = 0
+    position = "AAA"
+    while position != "ZZZ":
+        for left_right in parsed_data["instructions"]:
+            steps += 1
+            position = parsed_data["map"][position][left_right]
+            if position == "ZZZ":
+                break
+    return steps
+
+
+def lcm_of_list(numbers: list[int]) -> int:
+    """Lease common multiple of a list of numbers"""
+    return reduce(math.lcm, numbers)
+
+
+def part2(parsed_data: dict) -> int:
+    """Solve part 2 of the puzzle"""
+    positions = []
+    map_data = parsed_data["map"]
+    for currpos in map_data.keys():
+        if currpos[2] == "A":
+            positions.append(currpos)
+    results = []
+    for startposition in positions:
+        steps = 0
+        position = startposition
+        while position[2] != "Z":
+            for left_right in parsed_data["instructions"]:
+                steps += 1
+                position = parsed_data["map"][position][left_right]
+                if position[2] == "Z":
+                    break
+        results.append(steps)
+    return lcm_of_list(results)
+
+
+def part2_brute_force(parsed_data: dict) -> int:
+    """Solve part 2 of the puzzle using brute force method"""
+    steps = 0
+    positions = []
+    map_data = parsed_data["map"]
+    for currpos in map_data.keys():
+        if currpos[2] == "A":
+            positions.append(currpos)
+    proceed = True
+    while proceed:
+        for left_right in parsed_data["instructions"]:
+            steps += 1
+            print(f"Step {steps}")
+            proceed = False
+            for position in positions:
+                position = parsed_data["map"][position][left_right]
+                print(position[2])
+                if position[2] != "Z":
+                    proceed = True
+            if not proceed:
+                break
+    return steps
+
+
 def solve(input_value, part):
     """
     Function to solve puzzle
     """
-    return part
+    parsed_data = parse_input(input_value)
+    if part == 1:
+        return part1(parsed_data)
+    return part2(parsed_data)
 
 
 YEAR = 2023
 DAY = 8
 input_format = {
-    1: "lines",
-    2: "lines",
+    1: "text",
+    2: "text",
 }
 
 funcs = {
--- 2023/9/solution.py
+++ 2023/9/solution.py
@@ -18,11 +18,75 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def solve(input_value, part):
+def parse_input(lines: list[str]) -> list[list[int]]:
+    """Parse input data into list of lists of integers"""
+    # Split the data into lines
+    retval = []
+    # loop through lines
+    for line in lines:
+        int_data_list = []
+        for str_data in line.split():
+            int_data_list.append(int(str_data))
+        retval.append(int_data_list)
+    return retval
+
+
+def prev_value(data_list: list[int]) -> int:
+    """Get previous value in sequence"""
+    my_data = data_list.copy()
+    new_data = []
+    for i in range(0, len(my_data) - 1):
+        new_data.append(my_data[i + 1] - my_data[i])
+    allzeros = True
+    for i in new_data:
+        if i != 0:
+            allzeros = False
+    if not allzeros:
+        new_data.insert(0, prev_value(new_data))
+    return my_data[0] - new_data[0]
+
+
+def next_value(data_list: list[int]) -> int:
+    """Get next value in sequence"""
+    my_data = data_list.copy()
+    new_data = []
+    for i in range(0, len(my_data) - 1):
+        new_data.append(my_data[i + 1] - my_data[i])
+    allzeros = True
+    for i in new_data:
+        if i != 0:
+            allzeros = False
+    if not allzeros:
+        new_data.append(next_value(new_data))
+    return my_data[-1] + new_data[-1]
+
+
+def part1(parsed_data: list[list[int]]) -> int:
+    """Solve part 1 of puzzle"""
+    retval = 0
+    for int_data_list in parsed_data:
+        next_val = next_value(int_data_list)
+        retval += next_val
+    return retval
+
+
+def part2(parsed_data: list[list[int]]) -> int:
+    """Solve part 2 of puzzle"""
+    retval = 0
+    for int_data_list in parsed_data:
+        next_val = prev_value(int_data_list)
+        retval += next_val
+    return retval
+
+
+def solve(input_value: list[str], part: int) -> int:
     """
     Function to solve puzzle
     """
-    return part
+    parsed_data = parse_input(input_value)
+    if part == 1:
+        return part1(parsed_data)
+    return part2(parsed_data)
 
 
 YEAR = 2023
